4fa719f0420c647aaf10a5d127f44e0529bd75f6
Rusanov Andrey <andrey15rusanov@mail.ru>
2023-06-20 23:03:01 +0600

    первая версия

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  async getFilms(){
+    const res = await fetch (`https://api.themoviedb.org/3/search/movie?query=Naruto&api_key=dda4727764b73e18a45ed49f2d35ba07`)
+    const res1 = await res.json()
+    return res1.results
+  }
+
+  state = {
+    filmData: null
+  }
+
+  constructor(){
+    super();
+    this.addFilms();
+  }
+
+  addFilms(){
+    this.getFilms().then((arr) => {
+      this.setState({
+        filmData: arr
+      })
+    })
+  }
+
+  render() {
+    console.log(this.state.filmData)
+    let films = []
+    if (this.state.filmData != null){
+      for (let el of this.state.filmData){
+        let img = "https://image.tmdb.org/t/p/w500/" + el.poster_path
+        let genre = []
+        for (let n of el.genre_ids){
+          genre.push(<span>{n} </span>)
+        }
+        films.push(
+          <div key = {el.id} className="film">
+            <img src={img} className="poster"></img>
+          <div className="film_info">
+            <h1 className="title">{el.title}</h1>
+            <span className="date">{el.release_date}</span>
+              <div>{genre}</div>
+            <div className="discription">{el.overview}</div>
+          </div>
+        </div>
+        )
+      }
+    }
+   
+    return (
+      <div className="films">{films}</div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
